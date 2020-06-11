07423f41cf83fa77adb72b5947594e29097e45cc
ckdgus0204 <ckdgus0204@naver.com>
2020-06-11 21:21:52 +0900

    first

---
+++ website/accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import views
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import auth
+from .forms import CreateUserForm
+
+
+def signup(request):
+    if request.method == "POST":
+        userform = UserCreationForm(request.POST)
+        if userform.is_valid():
+            userform.save()
+            return redirect("home")
+    elif request.method=="GET":
+        userform = UserCreationForm()
+
+    return render(request,'registration/signup.html',{"userform":userform})
+
+def login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request,'registration/login.html')
+
+def logout(request):
+    if request.method=="POST":
+        auth.logout(request)
+        return redirct('home')
+    return render(request,'registration/login.html')
